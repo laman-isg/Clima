611bd04777496c72bd3b0a00787c335f5b3a2d68
Laman Isgandarova <104322290+laman-isg@users.noreply.github.com>
2023-09-18 23:07:39 +0400

    Add files via upload

---
+++ Clima/Model/WeatherManager.swift
@@ -0,0 +1,61 @@
+//
+//  WeatherManager.swift
+//  Clima
+//
+//  Created by Laman Isgandarova on 22.08.23.
+//  Copyright © 2023 App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=e0e7331c76ce7e7fcca46cbe26dcf6bb&units=metric"
+    
+    func fetchWeather(cityName: String) {
+        let urlString = "\(weatherURL)&q=\(cityName)"
+        performRequest(urlString: urlString)
+    }
+    
+    func performRequest(urlString: String) {
+        //1. Create a URL
+        if let url = URL(string: urlString) {
+            //2. Create a URLSession
+            let session = URLSession(configuration: .default)
+            //3. Give the session a task
+            let task = session.dataTask(with: url) { data, response, error in
+                if error != nil {
+                    print(error!)
+                    return
+                }
+                
+                if let safeData = data {
+                    parseJSON(weatherData: safeData)
+                }
+            }
+            //4. Start the task
+            task.resume()
+        }
+    }
+    
+    func parseJSON(weatherData: Data) {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
+            let id = decodedData.weather[0].id
+            let temp = decodedData.main.temp
+            let name = decodedData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            
+            print(weather.conditionName)
+        } catch {
+            print("error")
+        }
+        
+    }
+    
+    
+}
+
+
